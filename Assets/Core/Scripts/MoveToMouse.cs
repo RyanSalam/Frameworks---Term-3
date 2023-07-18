6c75b10842801461137e71676e74dee4d9fc2a66
RyanSalam <Ryan@agni.com>
2023-07-18 12:01:14 -0400

    Revert "Update MoveToMouse"
    This reverts commit b05e3b79f8091494de7b225d5b62b26f3e0df169.

--- Assets/Core/Scripts/MoveToMouse.cs
+++ Assets/Core/Scripts/MoveToMouse.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Jeremy was here, as well
-
 namespace FrameWorks.Core
 {
     public class MoveToMouse : MonoBehaviour
